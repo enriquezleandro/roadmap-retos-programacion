5f6b48cd5e8bfd5feee1570711fe794b741a9df9
enriquezleandro <enriquezleandro@gmail.com>
2024-11-15 13:46:56 -0300

    #00 - python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/enriquezleandro.py
@@ -0,0 +1,51 @@
+# https://www.python.org/
+
+# COMENTARIOS:
+
+# Comentario de una sola linea
+
+"""Comentario de 
+multiples lineas"""
+
+# Tipo de dato entero
+entero = 10
+
+# Tipo de dato flotante
+flotante = 10.5
+
+# Tipo de dato complejo
+complejo = 1 + 2j
+
+# Tipo de dato cadena de texto
+cadena = "Hola Mundo"
+
+# Tipo de dato booleano
+booleano = True
+
+# Tipo de dato lista
+lista = [1, 2, 3, 4, 5]
+
+# Tipo de dato tupla
+tupla = (1, 2, 3, 4, 5)
+
+# Tipo de dato conjunto
+conjunto = {1, 2, 3, 4, 5}
+
+# Tipo de dato diccionario
+diccionario = {"clave1": "valor1", "clave2": "valor2"}
+
+# Tipo de dato None
+nulo = None
+
+# Imprimir los valores de las variables
+print("Entero:", entero)
+print("Flotante:", flotante)
+print("Complejo:", complejo)
+print("Cadena:", cadena)
+print("Booleano:", booleano)
+print("Lista:", lista)
+print("Tupla:", tupla)
+print("Conjunto:", conjunto)
+print("Diccionario:", diccionario)
+print("None:", nulo)
+print("Hola Python")
